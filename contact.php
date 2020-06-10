54633de284ec17a0e928127a71de07a2716c0510
Nithya Nadaradjane <63090349+Clicknith@users.noreply.github.com>
2020-06-10 14:41:53 +0200

    Intitulé de la Nouvelle Fonctionalité

---
+++ contact.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+  	
+	
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1> <b>Shifted from Master Branch to New Contact Page Branch</b></h1>
+<header>
+ 	<nav>
+ 		<center>
+ 		     <ul>
+ 			<li><a href="Home.html"><b>Home</b></a></li>
+ 			<li><a href="Aboutus.html"><b>About us</b></a></li>
+ 			<li><a href="Gallery.html"><b>Gallery</b></a></li>
+ 			<li><a href="Contact.html"><b>Contact</b></a></li>
+            <li><a href="FAQ.html"<b>FAQ</b></a> </li>
+            
+ 		  </ul>
+ 		</center>
+ 	  </nav>	 
+</header>
+  <div class="RegisterForm">
+      <form>
+          <label>Sex</label>
+          <input type="radio"  name="Sex" > Male
+          <input type="radio" name="Sex" > Female
+          
+          <br><br>
+          <label>Last Name:</label>  
+          <input type="Text" name="Last Name" required>
+          <br>
+
+          <label>First Name:</label>  
+          <input type="Text" name="First Name">
+          <br>
+
+          <label>Email:</label><input type="email" name="Email">
+          <br>
+
+          <label>Date of Birth:</label>  
+          <input type="Date" name="Date">
+          <br>
+
+          <label>Address:</label>  
+          <input type="Text" name="Address">
+          <br>
+
+          <label>City:</label> 
+          <input type="Text" name="City">
+          <br>
+          
+          <label>ZIP Code:</label>  
+          <input type="Text" name="Zip Code" maxlength="9">
+          <br>
+
+          <label> Favorite Fruits:</label>
+          <input type="checkbox" name="Choose"> Bananas
+          <input type="checkbox" name="Choose"> Apples
+          <input type="checkbox" name="Choose">Oranges
+          <input type="checkbox" name="Choose">Strawberries
+          <input type="checkbox" name="Choose">Kiwis
+          <br><br>
+
+          <input type="Submit" value="Register">
+
+      </form>
+
+  </div>
+  <br>
+    <footer>
+     <address>
+     	FunGallery Main Office
+     	91 Rue d'Etas Unis
+		01500, Paris.
+		FRANCE.	
+     </address>
+             	           
+     <div class="Socials">
+
+     	<a href="Privacy Policy.html"target="_blank""Privacy Policy">Privacy Policy</a>
+     	<h4>
+     		<i> Follow us on </i>
+     	</h4>
+     	
+        <a href="https://www.facebook.com/" target="_blank"><img src="FB.png"></a>
+        <a href="https://twitter.com/explore" target="_blank"><img src="Twitter.png"></a>
+        <a href="https://www.instagram.com/" target="_blank"><img src="Insta.png"></a>
+        <a href="https://web.whatsapp.com/" target="_blank"><img src="Whatsapp.png"></a>
+        <a href="https://www.youtube.com/" target="_blank"><img src="Youtube.png"></a>
+     	         	
+     </div>
+    </footer>
+ 	 	
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
